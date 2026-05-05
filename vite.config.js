db777760277eebe79fee2b774f53edbd982ef7d7
Yuksel ONO <yuksel.ono@gmail.com>
2026-05-05 18:52:33 +0300

    fix: add id/name to all inputs

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/onox-quote-engine/' // 🔥 KRİTİK
+  base: '/onox-quote-engine/', // Burası deponuzun adıyla birebir aynı olmalı
 })
