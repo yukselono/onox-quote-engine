789159b30af5014d3ce4f501d1a45b9f69037208
Yuksel ONO <yuksel.ono@gmail.com>
2026-05-05 19:01:54 +0300

    fix: production ready build

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/onox-quote-engine/', // Burası deponuzun adıyla birebir aynı olmalı
+  base: '/onox-quote-engine/', // 🔥 EN KRİTİK
 })
